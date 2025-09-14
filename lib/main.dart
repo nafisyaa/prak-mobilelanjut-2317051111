1664a825a267a74a9d6c91a5759bb25e0c2fbbbc
nafisya.tyas11 <nafisya.tyas11@gmail.com>
2025-09-14 07:34:16 +0000

    Pertemuan 3: Basic Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+// import 'package:myapp/column_widget.dart';
+// import 'package:myapp/row_widget.dart';
+import 'package:myapp/basic_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,37 +10,26 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Praktikum Mobile lanjut')),
-        body : const Center(
-          child: Text('Halo Nama Saya Nafisya Yagtias ',
-          textAlign: TextAlign.center,
-          ),
-        ),
-      ),
+      // home: const ColumnWidget(),
+      // home: const RowWidget(),
+       home: const BasicWidget(),
+
+
+      // home: Scaffold(
+      //   appBar: AppBar(title: const Text('Praktikum Mobile lanjut')),
+      //   body : const Center(
+      //     child: Text('Halo Nama Saya Nafisya Yagtias ',
+      //     textAlign: TextAlign.center,
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
